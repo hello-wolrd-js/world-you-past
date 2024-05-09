ef60ac69fb50fb15ffbb106d60a0451ea2829d40
Tyee <flin00806@gmail.com>
2024-05-09 17:55:32 +0800

    feat(client): 弃用导航栏

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,16 +1,13 @@
 import { type JSXElement, type Component } from "solid-js";
-import { Nav } from "@/components/layout/Nav";
 import { Content } from "@/components/layout/Content";
 import { useNavigate } from "@solidjs/router";
 
 const App: Component<{ children?: JSXElement }> = (props) => {
-    const navHeight = 64;
     //这里要navigate一下,触发根路由
     useNavigate()("");
     return (
         <div class="w-full h-full overflow-x-hidden">
-            <Nav />
-            <Content navHeight={navHeight}>{props.children}</Content>
+            <Content>{props.children}</Content>
         </div>
     );
 };
--- packages/client/src/components/layout/Content.tsx
+++ packages/client/src/components/layout/Content.tsx
@@ -1,17 +1,18 @@
 import { Component, JSXElement } from "solid-js";
 
-export const Content: Component<{ navHeight: number; children: JSXElement }> = (
-    props
-) => {
+export const Content: Component<{
+    navHeight?: number;
+    children: JSXElement;
+}> = (props) => {
     //因为拿不到clientHeight所以只能这样
     const bodyHeight = import.meta.env.DEV
         ? document.body.clientHeight
         : parseInt(document.body.style.height);
-    const height = bodyHeight - props.navHeight;
+    const height = bodyHeight - (props.navHeight || 0);
     return (
         <main
             style={{
-                "margin-top": props.navHeight + "px",
+                "margin-top": (props.navHeight || 0) + "px",
                 height: height + "px",
             }}
         >

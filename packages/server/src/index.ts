ee46353544e7c27422ca4d8aca9b7e6494d5e16d
Nimi_Sora <2241073857@qq.com>
2024-05-12 20:35:52 +0800

    build(server):启用elysia和连接mongodb

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -1,6 +1,24 @@
-import { Elysia } from "elysia";
+import Elysia from 'elysia';
+import { MongoClient } from 'mongodb';
 
-const port = 3000;
-new Elysia().get("/", () => "hello!").listen(port);
+// MongoDB URI，通常存放于环境变量中
+const uri = "mongodb://0.0.0.0:27017/worldUpast";
+const client = new MongoClient(uri);
+const server = new Elysia().get("/", () => "test").listen(3000);
 
-console.log(`server is running on ${port}`);
+export class Db {
+  static async connect() {
+    const client = await MongoClient.connect(
+      uri,
+    )
+    return client.db('ts-mongodb')
+  }
+}
+
+process.on('SIGINT', async () => {
+  // 断开数据库连接
+  await client.close();
+  console.log('与MongoDB的连接已断开。');
+});
+
+export default server;

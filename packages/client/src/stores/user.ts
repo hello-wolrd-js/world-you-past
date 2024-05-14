b1a204069da5bbcb6bd9d5bb07d505f6f2f335f7
Tyee <flin00806@gmail.com>
2024-05-14 21:51:46 +0800

    fix(client/store): 封装初始化websocket的方法, 在用户信息初始化的时候调用

--- packages/client/src/stores/user.ts
+++ packages/client/src/stores/user.ts
@@ -4,26 +4,36 @@ import toast from "solid-toast";
 
 interface UserStoreState {
     user: User | null;
-    ws: WebSocket;
+    ws: WebSocket | null;
 }
-
-const _ws = new WebSocket(`wss://${import.meta.env.VITE_REMOTE}/game`);
-_ws.onopen = () => {
-    console.log("连接成功");
-};
-_ws.onerror = (e) => {
-    toast.error("连接服务器失败!");
-    console.error("连接服务器失败!", e);
-};
 
 const [store, setStore] = createStore<UserStoreState>({
     user: null,
-    ws: _ws,
+    ws: null,
 });
+
+const initWS = () => {
+    if (store.user) {
+        const _ws = new WebSocket(
+            `wss://${import.meta.env.VITE_REMOTE}/game?playerId=${
+                store.user.id
+            }`
+        );
+        _ws.onopen = () => {
+            console.log("连接成功");
+        };
+        _ws.onerror = (e) => {
+            toast.error("连接服务器失败!");
+            console.error("连接服务器失败!", e);
+        };
+        setStore("ws", _ws);
+    }
+};
 
 export const useUserStore = () => {
     return {
         state: store,
         setStore,
+        initWs: initWS,
     };
 };

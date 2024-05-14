5781a3f9924121f736b7a528c19846595b9cebbe
Tyee <flin00806@gmail.com>
2024-05-14 21:46:15 +0800

    feat(client/store): 初始化ws连接并将其嵌入至userStore

--- packages/client/src/stores/user.ts
+++ packages/client/src/stores/user.ts
@@ -1,12 +1,24 @@
 import { User } from "@world-you-past/models";
 import { createStore } from "solid-js/store";
+import toast from "solid-toast";
 
 interface UserStoreState {
     user: User | null;
+    ws: WebSocket;
 }
+
+const _ws = new WebSocket(`wss://${import.meta.env.VITE_REMOTE}/game`);
+_ws.onopen = () => {
+    console.log("连接成功");
+};
+_ws.onerror = (e) => {
+    toast.error("连接服务器失败!");
+    console.error("连接服务器失败!", e);
+};
 
 const [store, setStore] = createStore<UserStoreState>({
     user: null,
+    ws: _ws,
 });
 
 export const useUserStore = () => {

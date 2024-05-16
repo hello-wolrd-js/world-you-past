2de431858835033a773e3a5055636b9a98cd3c7b
Tyee <flin00806@gmail.com>
2024-05-16 17:03:07 +0800

    fix(server): 修复user.ts空导出

--- packages/server/src/services/user.ts
+++ packages/server/src/services/user.ts
@@ -1,24 +1,22 @@
-// import { db } from "@/db";
-// import { userDTO } from "@/models/user";
-// import {
-//     createSuccessResponse as SR,
-//     createErrorResponse as ER,
-// } from "@world-you-past/shared/response";
-// import Elysia from "elysia";
+import { db } from "@/db";
+import { userDTO } from "@/models/user";
+import {
+    createSuccessResponse as SR,
+    createErrorResponse as ER,
+} from "@world-you-past/shared/response";
+import Elysia from "elysia";
 
-// export const UserService = new Elysia().group(
-//     "/user",
-//     (app) => app
-//     //获取记录
-//     // app.get(
-//     //     "/record",
-//     //     async ({ body }) => {
-//     //         try {
-//     //             return SR(200, "保存记录到用户成功", await db.record);
-//     //         } catch (error) {
-//     //             return ER(-1, error);
-//     //         }
-//     //     },
-//     //     userDTO.record.create
-//     // )
-// );
+export const UserService = new Elysia().group("/user", (app) =>
+    app.get(
+        // 获取记录
+        "/record",
+        async () => {
+            try {
+                return SR(200, "获取用户记录成功", "test");
+            } catch (error) {
+                return ER(-1, error);
+            }
+        },
+        userDTO.record.create
+    )
+);
